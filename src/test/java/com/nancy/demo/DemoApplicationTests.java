f3005618d0da227ffc76408395a41fdb53c1de42
Nancy <nghazou35@gmail.com>
2026-01-30 13:44:26 -0400

    first commit - archivos iniciales

---
+++ src/test/java/com/nancy/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nancy.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
